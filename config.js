2ad7aa85702d7a8e2ac65f3a7feccbe4d2a3ff2d
CMTV <cmtvpetya@gmail.com>
2020-02-26 17:36:41 +0300

    Pre release 10

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ let CNAME = 'bydef.ru';
 module.exports = {
     devMode: devMode,
     url: (global.out) ? url : 'http://localhost:8080/',
+    CNAME: CNAME,
     links:
     {
         donate: 'https://boosty.to/bydef',
