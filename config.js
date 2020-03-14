b0721300be67e918482dc58249d8defd2d92bf52
CMTV <cmtvpetya@gmail.com>
2020-03-14 16:38:02 +0300

    Фикс включенного на проде devMode

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let url = 'https://bydef.ru/';
 let CNAME = 'bydef.ru';
 
 module.exports = {
-    devMode: devMode,
+    devMode: (global.out) ? false : devMode,
     url: (global.out) ? url : 'http://localhost:8080/',
     CNAME: CNAME,
     links:
